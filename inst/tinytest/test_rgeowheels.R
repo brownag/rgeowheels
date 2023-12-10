856208d5f1cef3f59d9daf149bc6d20a33c8470f
Andrew G. Brown <andrew.g.brown@usda.gov>
2023-12-10 09:27:13 -0800

    Add some small tests

---
+++ inst/tinytest/test_rgeowheels.R
@@ -0,0 +1,9 @@
+x <- try(list_assets())
+if (!inherits(x, 'try-error')) {
+  expect_true(inherits(x, 'data.frame'))
+} else message("getting asset list from GitHub releases failed")
+
+y <- try(install_wheel("GDAL", url_only = TRUE))
+if (!inherits(y, 'try-error')) {
+  expect_true(is.character(y))
+} else message("getting latest GDAL wheel URL failed")
